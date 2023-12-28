844cafd20986128cf91d9b97eb3e3d4e83bb4f90
Leovander <leovanderadds@gmail.com>
2023-12-28 20:22:42 +0700

    Leo update

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -1,35 +1,104 @@
+import wave
+import sys
+import pyaudio
 import speech_recognition as sr
-import pyttsx3
+import RPi.GPIO as GPIO
 
-# Inisialisasi recognizer
-recognizer = sr.Recognizer()
+#Konfigurasi record input
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 44100
+RECORD_SECONDS = 5
+OUTPUT_FILE = 'output.wav'
 
-# Inisialisasi engine TTS
-engine = pyttsx3.init()
+# Tentukan pin GPIO yang terhubung dengan relay
+RELAY_PIN_1 = 18
+RELAY_PIN_2 = 27
+RELAY_PIN_3 = 22
+RELAY_PIN_4 = 23
 
-# Menggunakan mikrofon sebagai sumber suara
-with sr.Microphone() as source:
-    print("Silakan bicara...")
-    audio = recognizer.listen(source)
+# Konfigurasi GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN_1, GPIO.OUT)
+GPIO.setup(RELAY_PIN_2, GPIO.OUT)
+GPIO.setup(RELAY_PIN_3, GPIO.OUT)
+GPIO.setup(RELAY_PIN_4, GPIO.OUT)
 
-    try:
-        recognized_text = recognizer.recognize_google(audio)
-        print("Anda mengatakan: " + recognized_text)
+# Fungsi untuk melakukan transkripsi
+def transcribe_audio(audio_file_path):
+    recognizer = sr.Recognizer()
 
-        if "hello" in recognized_text:
-            response_text = "Halo! Bagaimana saya dapat membantu Anda?"
-        elif "Terima kasih" in recognized_text:
-            response_text = "Tidak masalah. Semoga harimu menyenangkan."
-        else:
-            response_text = "Saya tidak mengerti apa yang Anda katakan."
+    with sr.AudioFile(audio_file_path) as source:
+        audio_data = recognizer.record(source)  # Merekam audio dari file
 
-        print("Respons: " + response_text)
+        try:
+            # Menggunakan recognizer dengan bahasa Indonesia
+            text = recognizer.recognize_google(audio_data, language='id-ID')
+            print("Program menyatakan", text)
 
-        # Memainkan respons suara
-        engine.say(response_text)
-        engine.runAndWait()
+            # Menambahkan kondisi berdasarkan hasil transkripsi
+            if 'nyalakan kipas' in text.lower():
+                GPIO.output(RELAY_PIN_1, GPIO.HIGH)
+                print("Kipas menyala.")
+            elif 'matikan kipas' in text.lower():
+                GPIO.output(RELAY_PIN_1, GPIO.LOW)
+                print("Kipas mati.")
+                
+            if 'nyalakan lampu belakang' in text.lower():
+                GPIO.output(RELAY_PIN_2, GPIO.HIGH)
+                print("Lampu belakang menyala.")
+            elif 'matikan lampu belakang' in text.lower():
+                GPIO.output(RELAY_PIN_2, GPIO.LOW)
+                print("Lampu belakang mati.")
+                
+            if 'nyalakan lampu depan' in text.lower():
+                GPIO.output(RELAY_PIN_3, GPIO.HIGH)
+                print("Lampu depan menyala.")
+            elif 'matikan lampu depan' in text.lower():
+                GPIO.output(RELAY_PIN_3, GPIO.LOW)
+                print("Lampu depan mati.")
+                
+            if 'nyalakan lampu tengah' in text.lower():
+                GPIO.output(RELAY_PIN_4, GPIO.HIGH)
+                print("Lampu tengah menyala..")
+            elif 'matikan lampu tengah' in text.lower():
+                GPIO.output(RELAY_PIN_4, GPIO.LOW)
+                print("Lampu tengah mati.")
+                
+            # Menambahkan kondisi untuk mematikan semua lampu
+            if 'nyalakan semua lampu' in text.lower():
+                GPIO.output(RELAY_PIN_2, GPIO.HIGH)
+                GPIO.output(RELAY_PIN_3, GPIO.HIGH)
+                GPIO.output(RELAY_PIN_4, GPIO.HIGH)
+                print("Semua lampu dimatikan.")
+            if 'matikan semua lampu' in text.lower():
+                GPIO.output(RELAY_PIN_2, GPIO.LOW)
+                GPIO.output(RELAY_PIN_3, GPIO.LOW)
+                GPIO.output(RELAY_PIN_4, GPIO.LOW)
+                print("Semua lampu dimatikan.")
+                
+        except sr.UnknownValueError:
+            print("Google Web API tidak dapat mengenali audio")
+        except sr.RequestError as e:
+            print("Terjadi kesalahan pada Google Web API; {0}".format(e))
 
-    except sr.UnknownValueError:
-        print("Tidak dapat mengenali suara")
-    except sr.RequestError as e:
-        print("Error pada permintaan suara: {0}".format(e))
+# Merekam suara
+with wave.open(OUTPUT_FILE, 'wb') as wf:
+    p = pyaudio.PyAudio()
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+
+    print('Katakan Sesuatu...')
+    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
+    print('Selesai')
+
+    stream.close()
+    p.terminate()
+
+# Melakukan transkripsi pada file output
+transcribe_audio(OUTPUT_FILE)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import time
 import cv2
 import HandTrackingModule as htm
+import RPi.GPIO as GPIO
+
+# Set mode pin numbering pada GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+# Inisialisasi relay
+relay_pin_numbers = {1: 18, 2: 27, 3: 22, 4: 23}  # Sesuaikan dengan nomor pin GPIO yang terhubung ke relay
+for pin in relay_pin_numbers.values():
+    GPIO.setup(pin, GPIO.OUT)
 
 wCam, hCam = 640, 480
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
@@ -15,6 +25,11 @@ tipIds = [4, 8, 12, 16, 20]
 state = ""
 while True:
     success, img = cap.read()
+
+    # Check if the image is not empty
+    if not success or img is None:
+        continue
+
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
 
@@ -33,37 +48,44 @@ while True:
                 fingers.append(0)
 
         totalFingers = fingers.count(1)
-        
+
         if totalFingers == 1:
             cv2.putText(img, "Satu Jari Terpilih", (50, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            state = "satu"
+            state = 1
         elif totalFingers == 2:
             cv2.putText(img, "Dua Jari Terpilih", (50, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            state = "dua"
+            state = 2
         elif totalFingers == 3:
             cv2.putText(img, "Tiga Jari Terpilih", (50, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            state = "tiga"
+            state = 3
         elif totalFingers == 4:
             cv2.putText(img, "Empat Jari Terpilih", (50, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            state = "empat"
+            state = 4
         elif totalFingers == 5 and state != "":
-            print("Relay " + state + " nyala")
+            relay_pin = relay_pin_numbers.get(state)  # Get the actual GPIO pin number
+            if relay_pin is not None:  # Check if the relay_pin is found
+                GPIO.output(relay_pin, GPIO.HIGH)
+                print("Relay " + str(state) + " nyala")
             state = ""
         elif totalFingers == 0 and state != "":
-            print("Relay " + state + " dimatikan")
+            relay_pin = relay_pin_numbers.get(state)  # Get the actual GPIO pin number
+            if relay_pin is not None:  # Check if the relay_pin is found
+                GPIO.output(relay_pin, GPIO.LOW)
+                print("Relay " + str(state) + " dimatikan")
             state = ""
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    
-    cv2.putText(img, "Terpilih " + state, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.putText(img, "Terpilih " + str(state), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
                 3, (255, 0, 0), 3)
-    
+
     cv2.imshow("Image", img)
     if cv2.waitKey(10) & 0xFF == 27:  # Keluar dari loop saat tombol Esc ditekan
-            break
-    
+        break
+
 cap.release()
 cv2.destroyAllWindows()
+GPIO.cleanup()  # Bersihkan GPIO setelah selesai
